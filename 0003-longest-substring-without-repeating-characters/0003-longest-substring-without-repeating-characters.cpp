bc96a55535e8abbea76e255fd556eaafa11d202b
Pankajesc <pankajkumaryadavv87@gmail.com>
2023-09-18 21:39:48 +0530

    DSA

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -6,12 +6,11 @@ public:
         int i=0,j=0,ans=INT_MIN; 
         while(j<s.length())   
         {
-            m[s[j]]++;  //increase the frequency of the element as you traverse the string
-            if(m.size()==j-i+1)  // whem map size is equal to the window size means suppose window size is 3 and map size is also three that means in map all unique characters are their
+            m[s[j]]++;  
+            if(m.size()==j-i+1) 
             {
-                ans = max(ans,j-i+1);  //compare the length of the maximum window size
-            }
-            else if(m.size()<j-i+1)   //if the map size is less than the window size means there is some duplicate present like window size = 3 and map size = 2 means there is a duplicates
+                ans = max(ans,j-i+1); 
+            else if(m.size()<j-i+1)   
             {
                 while(m.size()<j-i+1)  //so till the duplicates are removed completely
                 {
