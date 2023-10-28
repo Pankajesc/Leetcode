a1c930f49555f4ab704e033fb402d4239d43f2a7
Pankaj Kumar Yadav <125720698+Pankajesc@users.noreply.github.com>
2023-10-28 19:08:16 +0530

    Time: 5 ms (33.75%), Space: 8.2 MB (29.84%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> res;
+        for(int i=1;i<=n;i++){
+            if(i%3!=0 && i%5!=0){
+                string a = to_string(i);
+                res.push_back(a);
+            }
+            if(i%3==0 && i%5==0){
+                res.push_back("FizzBuzz");
+            }
+            else if(i%3==0){
+                res.push_back("Fizz");
+            }
+            else if(i%5==0){
+                res.push_back("Buzz");
+            }
+        }
+
+        return res;
+    }
+};
