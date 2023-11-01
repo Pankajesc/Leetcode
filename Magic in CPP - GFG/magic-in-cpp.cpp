ee323f49380f274465e74c0b36622a0e067aa4ed
Pankaj Kumar Yadav <125720698+Pankajesc@users.noreply.github.com>
2023-11-01 14:42:33 +0530

    Added solution - LeetHub

---
+++ Magic in CPP - GFG/magic-in-cpp.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <iostream>
+using namespace std;
+
+// Function prototype
+void swap(int*, int*);
+void swap(int&, int&);
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+// Function to swap A and B
+// using pointers
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Function to swap two numbers using references
+void swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+
+//{ Driver Code Starts.
+
+// Driver code
+int main() {
+	
+	int testcase;
+	cin >> testcase;
+	
+	while(testcase--){
+	    int A, B;
+	    cin >> A >> B;
+	    
+	    // Swapping by reference
+	    swap(A, B);
+	    cout << A << " " << B << endl;
+	    
+	    // Swap by pointers
+	    swap(&A, &B);
+	    
+	    cout << A << " " << B << endl;
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
