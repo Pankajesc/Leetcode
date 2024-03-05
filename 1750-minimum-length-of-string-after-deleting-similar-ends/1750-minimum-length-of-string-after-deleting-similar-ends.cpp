70d40d4aabf9ac7c47d4fad7236a79107a189b10
Pankaj Kumar Yadav <125720698+Pankajesc@users.noreply.github.com>
2024-03-05 21:48:08 +0530

    Time: 12 ms (99.31%), Space: 13.8 MB (70.34%) - LeetHub

---
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumLength(string str) {
+        ios_base::sync_with_stdio(0);
+        int s=0,e=str.size()-1; //s=start,e=end
+        while(s<e){
+            if(str[s]==str[e]){
+                while(str[s]==str[s+1] && s<e)s++;
+                while(str[e]==str[e-1] && s<e)e--;
+                s++;
+                e--;
+            }
+            else break;
+        }
+        int ans=e-s+1; //ans=remaining array length
+        if(ans<0)return 0;
+        return ans;
+    }
+};
